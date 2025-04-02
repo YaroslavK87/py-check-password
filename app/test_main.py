7d961588b99112928687f18de0c027250ccd6a6f
Yaroslav Kalugin <yaroslav.kalugin.07@gmail.com>
2025-04-02 16:06:41 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,21 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, expected", [
+        ("Pass@word1", True),
+        ("Abc@1234", True),
+        ("Valid123!", True),
+        ("Test@Pass1", True),
+        ("qwerty", False),
+        ("Str@ng", False),
+        ("Password@!", False),
+        ("NoSpecial123", False),
+        ("password123!", False),
+        ("A1!thisIsALongPassword123!", False),
+        ("Invalid!#*", False),
+    ]
+)
+def test_check_password(password: str, expected: str) -> None:
+    assert check_password(password) == expected
